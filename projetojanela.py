1de2ba1deefcfe5b8189d5cddd8469306c495c3f
Leandro Alves <128817039+Almirante2023@users.noreply.github.com>
2023-07-02 20:13:02 -0300

    Update projetojanela.py
    2 janelas

--- projetojanela.py
+++ projetojanela.py
@@ -1,8 +1,29 @@
+
 import customtkinter
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
+# Função para abrir a segunda janela em uma nova aba
+def abrir_segunda_janela():
+    janela_segunda_aba = customtkinter.CTk()
+    janela_segunda_aba.geometry("600x300")
+
+    texto2 = customtkinter.CTkLabel(janela_segunda_aba, text="Coletar Dados")
+    texto2.pack(padx=10, pady=10)
+
+    cpf = customtkinter.CTkEntry(janela_segunda_aba, placeholder_text="Seu CPF")
+    cpf.pack(padx=10, pady=10)
+
+    email2 = customtkinter.CTkEntry(janela_segunda_aba, placeholder_text="Seu e-mail")
+    email2.pack(padx=10, pady=10)
+
+    telefone = customtkinter.CTkEntry(janela_segunda_aba, placeholder_text="Seu telefone")
+    telefone.pack(padx=10, pady=10)
+
+    janela_segunda_aba.mainloop()
+
+# Janela principal
 janela = customtkinter.CTk()
 janela.geometry("600x300")
 
@@ -10,7 +31,7 @@ texto = customtkinter.CTkLabel(janela, text="Fazer Login")
 texto.pack(padx=10, pady=10)
 
 def clique():
-    print("Botão clicado")
+    abrir_segunda_janela()  # Chama a função para abrir a segunda janela em uma nova aba
 
 email = customtkinter.CTkEntry(janela, placeholder_text="Seu e-mail")
 email.pack(padx=10, pady=10)
@@ -21,8 +42,8 @@ senha.pack(padx=10, pady=10)
 checkbox = customtkinter.CTkCheckBox(janela, text="Lembrar Login")
 checkbox.pack(padx=10, pady=10)
 
-botao = customtkinter.CTkButton(janela, text="Login", command=clique())
+botao = customtkinter.CTkButton(janela, text="Login", command=clique)
 botao.pack(padx=10, pady=10)
 
-
 janela.mainloop()
+
